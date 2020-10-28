374c425dd5e26275d4968a637a8c8b26684d71ee
Zachariah Crowell <zachariah.crowell@techtonic.com>
2020-10-27 22:54:28 -0600

    Add badges, and sort by priority level

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import "./Styles/App.css";
 
 function App() {
   const [todos, setTodos] = useState([
-    { name: "Task 1", done: false, id: 1, priority: "Low" },
-    { name: "Task 2", done: false, id: 2, priority: "Low" },
-    { name: "Task 3", done: false, id: 3, priority: "Low" },
-    { name: "Task 4", done: false, id: 4, priority: "Low" },
-    { name: "Task 5", done: false, id: 5, priority: "Low" },
-    { name: "Task 6", done: false, id: 6, priority: "Low" },
-    { name: "Task 7", done: false, id: 7, priority: "Low" },
-    { name: "Task 8", done: false, id: 8, priority: "Low" },
+    { name: "Task 1", done: false, id: 1, priority: 3 },
+    { name: "Task 2", done: false, id: 2, priority: 3 },
+    { name: "Task 3", done: false, id: 3, priority: 3 },
+    { name: "Task 4", done: false, id: 4, priority: 3 },
+    { name: "Task 5", done: false, id: 5, priority: 2 },
+    { name: "Task 6", done: false, id: 6, priority: 2 },
+    { name: "Task 7", done: false, id: 7, priority: 2 },
+    { name: "Task 8", done: false, id: 8, priority: 1 },
   ]);
   const [tab, setTab] = useState("Pending");
 
@@ -24,11 +24,19 @@ function App() {
     newTodos[index].done = true;
     setTodos(newTodos);
   };
+  const changePriority = (index, priorityLevel) => {
+    const newTodos = [...todos];
+    newTodos[index].priority = priorityLevel;
+    setTodos(newTodos.sort((a, b) => a.priority - b.priority));
+
+  };
 
   return (
     <div className="app">
       <div className="main">
-        <div className="tab-group">
+
+    <div style={{border: '2px solid black', display: 'flex', justifyContent: 'center'}}>
+        <div style={{display: 'flex', border: '2px solid pink', width: '90%'}}>
           <p className={tab === "Pending" ? "tab-group__tabs bold" : 'tab-group__tabs '} 
           onClick={() => changeTab("Pending")}>
             Pending
@@ -38,11 +46,11 @@ function App() {
             onClick={() => changeTab("Completed")}>
             Completed
           </p>
+       </div>
         </div>
 
-       <div style={{display: 'flex'}}>
-
-       <div className="todo-list-group" style={{width: '75%'}}>
+       <div style={{display: 'flex', justifyContent: 'center'}}>
+       <div className="todo-list-group" >
           {tab === "Pending"
             ? todos.map((x, i) => {
                 if (x.done === false) {
@@ -53,6 +61,7 @@ function App() {
                       todo={x}
                       i={i}
                       changeToDo={changeToDo}
+                      changePriority={changePriority}
                     ></Todo>
                   );
                 }
@@ -66,27 +75,28 @@ function App() {
                       todo={x}
                       i={i}
                       changeToDo={changeToDo}
+                      changePriority={changePriority}
                     ></Todo>
                   );
                 }
               })}
         </div>
 
-        <div style={{border: '2px solid red', width: '25%'}}>
-          <div style={{border: '2px solid pink', margin: '10px'}}>
-            <p>
+        <div className="date-todo-count-container">
+          <div className="date-card">
+            <p className="date-card-day">
             Wednesday
             </p>
-            <p>
+            <p className="date-card-month-year">
             November, 21
             </p>
-            <p>
+            <p className="date-card-year">
             2020
             </p>
           </div>
-          <div style={{border: '2px solid blue', margin: '10px', width: '50%'}}>
-            Todos:
-            {todos.length}
+          <div className="todo-count-card">
+            <p className="todo-count">{todos.length}</p>
+            <p className="todo-tasks">Tasks</p>
           </div>
         </div>
 
--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -5,20 +5,37 @@ import checked from '../Assets/checked.png'
 import calbutton from '../Assets/calbutton.png'
 import cancel from '../Assets/cancel.png'
 
-export default function Todo({changeToDo, todo, i}) {
+export default function Todo({changeToDo, changePriority, todo, i}) {
+    // Decides color of chip when priorty is selected
+    const chipColor = (priorityColor, todoPrority) => {
+        let colorOfChip = 'no-chip-color';
+        if(priorityColor === todoPrority){
+            colorOfChip = `chip-color-${todoPrority}`
+        }
+        return colorOfChip
+    }
+
     return (
         <div className="todo-container">
+
             <div className="todo-group">
                 <div className="todo-text-icon">
                  <img onClick={() => changeToDo(i)} src={todo.done ? checked : unchecked} alt="unchecked box"/>
                   <span className="todo-name">{todo.name}</span>
                 </div>
+
                 <div className="todo-text-icon">
                     <img src={cancel}/>
-                      <span className="cal-group">Nov 7th, 2020</span>
+                      <span className="cal-group cal-date">Nov 7th, 2020</span>
                     <img className="cal-group" src={calbutton}/>
                 </div>
             </div>
+
+           <div className="chip-levels-group">
+               <p className={chipColor(3, todo.priority)} onClick={() => changePriority(i, 3)}>Low</p>
+               <p className={chipColor(2, todo.priority)} onClick={() => changePriority(i, 2)}>Medium</p>
+               <p className={chipColor(1, todo.priority)} onClick={() => changePriority(i, 1)}>High</p>
+           </div>
         </div>
     )
 }
